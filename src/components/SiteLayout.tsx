653c3205816fae2b6e481fa02258cf2fe1411ff7
B SIMAK AHMED <1NZ24CS030@newhorizonindia.edu>
2026-09-21 12:59:54 +0530

    Remove sticky mobile contact CTA

--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -1,15 +1,13 @@
 import type { ReactNode } from "react";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
-import { StickyMobileCta } from "./StickyMobileCta";
 
 export function SiteLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex min-h-screen flex-col">
       <Navbar />
-      <main className="flex-1 pb-20 sm:pb-0">{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
-      <StickyMobileCta />
     </div>
   );
 }
